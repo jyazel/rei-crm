b7ce2d3e62aa2ef9b5e60eb34146579a0e410e65
jyazel <jeff@crusaderproperties.com>
2026-04-19 18:16:28 -0400

    add client/tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: { colors: { primary: { 50:'#fff5f0',100:'#ffe4d6',500:'#f97316',600:'#ea6c0c',700:'#c2570a' } } } },
+  plugins: [],
+};
