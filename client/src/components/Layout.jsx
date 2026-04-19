86b4e3a1c0968240bf847b7c6e1cbaaed71ac013
jyazel <jeff@crusaderproperties.com>
2026-04-19 18:17:44 -0400

    add client/src/components/Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -4,9 +4,7 @@ export default function Layout() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <main className="flex-1 overflow-auto bg-[#faf9f7]">
-        <Outlet />
-      </main>
+      <main className="flex-1 overflow-auto bg-[#faf9f7]"><Outlet /></main>
     </div>
   );
 }
