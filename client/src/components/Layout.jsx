fb8a901d06d7d31930fa3af868f72e88b2ecb7c9
jyazel <jeff@crusaderproperties.com>
2026-04-19 18:17:19 -0400

    add client/src/components/Layout.jsx

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+export default function Layout() {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <main className="flex-1 overflow-auto bg-[#faf9f7]">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
